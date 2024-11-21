9fa748d256da90cff942d300ae454a4da844c65e
Michał Bielicki <mbielicki2912@gmail.com>
2024-11-21 23:19:34 +0100

    constant rtt control block address, not working

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
-import serial
+# "J-Link OB-nRF5340-NordicSemi"
+# 1050202199
 
-ser = serial.Serial(
-    port='COM14',
-    baudrate=115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=0
-)
+import pylink
+import time
 
-print("Connected to:", ser.portstr)
+# Constants
+RTT_CONTROL_BLOCK_ADDRESS = 0x200000a4
+DEVICE_NAME = "nRF52840_xxAA"  # Device name for the nRF52840
+SERIAL_NO = 1050202199
+RTT_BUFFER_SIZE = 1024  # Size of the RTT buffer to read
 
-array_tmp = []
+def read_rtt(jlink: pylink.JLink, rtt_address):
+    """
+    Read RTT data from the target device.
+    """
+    
+    jlink.rtt_start(RTT_CONTROL_BLOCK_ADDRESS)
+    print(jlink.rtt_get_num_up_buffers())
 
-while True:
-    byte_read = ser.read()  # Read bytes from serial port
-    if byte_read:#if not empty
-        byte_char = byte_read.decode()  # Decode bytes to string
-        array_tmp.append(byte_char)
-        if byte_char == '\n':
-            print(''.join(array_tmp))  # Print the received line
-            print(array_tmp)  # Print the received line
-            array_tmp = []
+def main():
+    # Initialize J-Link
+    jlink = pylink.JLink()
+    jlink.open(SERIAL_NO)
+    jlink.set_tif(pylink.enums.JLinkInterfaces.SWD)
+    jlink.connect(DEVICE_NAME)
 
-ser.close()
+    print(jlink.connected())
+
+    # Halt the CPU to access memory safely
+    # jlink.halt()
+
+    # Start reading RTT data
+    print("Reading RTT data...")
+    read_rtt(jlink, RTT_CONTROL_BLOCK_ADDRESS)
+
+if __name__ == "__main__":
+    main()
